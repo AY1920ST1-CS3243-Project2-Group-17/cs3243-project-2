513393d0759233285c8f2c9fa74ec42fe4d12014
Neo Weihong <whneo97@hotmail.com>
2020-05-29 03:54:07 +0800

    Implement O(logn) remove function for Priority Queue.

--- minheap.py
+++ minheap.py
@@ -32,25 +32,46 @@ class MinHeap:
     def right(self, i):
         return 2*i + 1
 
-    def update(self, old_key, new_key=None):
-        if new_key is None:
-            new_key = old_key
-        index = self.key_index_map[old_key][0]
+    def update(self, old_key, new_key):
+        index = self.key_index_map[old_key][-1]
         try:
             self.key_index_map[new_key].append(index)
         except KeyError:
             self.key_index_map[new_key] = [index]
         self.pq[index] = new_key
+        try:
+            self.key_index_map[old_key].pop()
+        except IndexError:
+            del self.key_index_map[old_key]
         self.shift_up(index)
         self.shift_down(index)
+    
+    def remove(self, key):
+        # print('Before remove', self.key_index_map)
+        index = self.key_index_map[key][-1]
+        self.key_index_map[self.pq[-1]][-1] = index
+        try:
+            self.key_index_map[key].pop()
+        except IndexError:
+            del self.key_index_map[key]
+        # print(self.BinaryHeapSize, index, 'heap size and index')
+        # print(self.pq[index], 'pq index item')
+        # print(self.pq[-1], 'last pq item')
+        self.pq[index], self.pq[-1] = self.pq[-1], self.pq[index]   
+        # print('Mid remove', self.key_index_map)
+        self.pq.pop()
+        self.BinaryHeapSize -= 1
+        if index < self.BinaryHeapSize:
+            self.shift_up(index)
+            self.shift_down(index)
   
     def shift_up(self, i):
         f = self.comparator
         while (i > 1 and (f(self.pq[self.parent(i)]) > f(self.pq[i])
                           if self.minheap else f(self.pq[self.parent(i)]) < f(self.pq[i]))):
             self.pq[i], self.pq[self.parent(i)] = self.pq[self.parent(i)], self.pq[i]
-            self.key_index_map[self.pq[i]][0], self.key_index_map[self.pq[self.parent(i)]][0] = (
-                self.key_index_map[self.pq[self.parent(i)]][0], self.key_index_map[self.pq[i]][0])
+            self.key_index_map[self.pq[i]][-1], self.key_index_map[self.pq[self.parent(i)]][-1] = (
+                self.key_index_map[self.pq[self.parent(i)]][-1], self.key_index_map[self.pq[i]][-1])
             i = self.parent(i)
     
     def push(self, key):
@@ -85,8 +106,8 @@ class MinHeap:
         
             if max_id != i:
                 self.pq[i], self.pq[max_id] = self.pq[max_id], self.pq[i]
-                self.key_index_map[self.pq[i]][0], self.key_index_map[self.pq[max_id]][0] = (
-                    self.key_index_map[self.pq[max_id]][0], self.key_index_map[self.pq[i]][0])
+                self.key_index_map[self.pq[i]][-1], self.key_index_map[self.pq[max_id]][-1] = (
+                    self.key_index_map[self.pq[max_id]][-1], self.key_index_map[self.pq[i]][-1])
                 i = max_id
             else:
                 break
@@ -94,7 +115,11 @@ class MinHeap:
     def pop(self):
         extreme_v=0
         if self.BinaryHeapSize != 0:
-            extreme_v = self.pq[1]    
+            extreme_v = self.pq[1]
+            try:
+                self.key_index_map[extreme_v].pop()
+            except IndexError:
+                del self.key_index_map[extreme_v]
             self.pq[1] = self.pq[self.BinaryHeapSize]
             self.BinaryHeapSize -= 1
             self.shift_down(1)
@@ -127,14 +152,44 @@ class MinHeap:
         return self.BinaryHeapSize != 0
 
 heap = MinHeap(comparator=lambda x: x[0])
-unsorted = [(1, 3), (2, 1), (1, 4), (2, 0), (1, 5), (2, -1), (2, -3), (3, -1)]
+unsorted = [(10, 3, 4), (2, 1), (1, 4), (2, 0), (1, 5), (2, -1), (2, -3), (3, -1)]
 [heap.push(i) for i in unsorted]
+
 ls = []
-heap.update((2, 1), (2, 10))
+# heap.update((2, 1), (2, 10))
 heap.push((2, 10))
+
 # heap.update(2, 11)
 heap.push((3, -1))
+
+print(heap.pq)
+
 heap.update((3, -1), (5, 11))
+
+heap.update((3, -1), (5, 1))
+
+heap.push((3, -1))
+
+heap.update((3, -1), (2, 1))
+
+# heap.remove((2, -1))
+# print(heap.pq, 'hello')
+# print(heap.key_index_map)
+
+heap.remove((2, 1))
+# print(heap.pq)
+# print(heap.key_index_map)
+
+
+heap.remove((2, 1))
+
+
+heap.remove((5, 11))
+
+
+heap.remove((5, 1))
+
+# heap.update((2, -3), (5, 11))
 
 # heap.update(2, 12)
 # heap.update(1, 13)
