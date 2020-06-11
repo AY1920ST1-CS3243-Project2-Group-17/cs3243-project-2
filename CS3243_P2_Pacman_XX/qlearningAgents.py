ade3cb61f82e89d1b590ec47ccc3e9e7ecd72cf4
Neo Weihong <whneo97@hotmail.com>
2020-06-11 23:46:31 +0800

    Rewrite dictionary comprehension for python2.6.

--- CS3243_P2_Pacman_XX/qlearningAgents.py
+++ CS3243_P2_Pacman_XX/qlearningAgents.py
@@ -82,8 +82,8 @@ class QLearningAgent(ReinforcementAgent):
         if not self.getLegalActions(state):
             return None
 
-        actionQValues = {action: self.getQValue(state, action) 
-                         for action in self.getLegalActions(state)}
+        actionQValues = dict((action, self.getQValue(state, action)) 
+                             for action in self.getLegalActions(state))
         bestActions = [action for action, qValue in actionQValues.items()
                        if (qValue == max(actionQValues.values()))]
           
